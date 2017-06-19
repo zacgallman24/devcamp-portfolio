c47b1ff9755180971bc1f61b7bf9815a91fc2f39
Jordan Hudgens <jordan@devcamp.com>
2017-06-19 16:41:22 +0000

    Added portfolio items via resource generator

--- DevcampPortfolio/config/routes.rb
+++ DevcampPortfolio/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :portfolios
   get 'pages/about'
 
   get 'pages/contact'
--- DevcampPortfolio/db/schema.rb
+++ DevcampPortfolio/db/schema.rb
@@ -10,13 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170619154110) do
+ActiveRecord::Schema.define(version: 20170619163509) do
 
   create_table "blogs", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "portfolios", force: :cascade do |t|
+    t.string   "title"
+    t.string   "subtitle"
+    t.text     "body"
+    t.text     "main_image"
+    t.text     "thumb_image"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "skills", force: :cascade do |t|

c62f32f7762e49b9d7189ec17f8669caf57b3f82
Jordan Hudgens <jordan@devcamp.com>
2017-06-19 16:24:37 +0000

    Integrated skills via model generator

--- DevcampPortfolio/db/schema.rb
+++ DevcampPortfolio/db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170613172754) do
+ActiveRecord::Schema.define(version: 20170619154110) do
 
   create_table "blogs", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "percent_utilized"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
   end
 
 end

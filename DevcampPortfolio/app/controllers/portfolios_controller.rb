15afe2637f2980808a48963994e4ef6fcb2a4d7f
Jordan Hudgens <jordan@devcamp.com>
2017-06-21 14:51:18 +0000

    Integrated index action for portfolio-items

--- DevcampPortfolio/app/controllers/portfolios_controller.rb
+++ DevcampPortfolio/app/controllers/portfolios_controller.rb
@@ -1,2 +1,8 @@
-class PortfoliosController < ApplicationController
+class PortfoliosController < ApplicationController 
+  def index
+    @portfolio_items=Portfolio.all
+  <p><%= portfolio_items.title %><p>
+  <p><%= portfolio_items.subtitle %><p>
+  <p><%= portfolio_items.body %><p>
+  <%= image_tag portfolio_items.thumb_image %>
 end

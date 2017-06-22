ecb2c73d28cd85cf6cde8d919eac29d036ad6a19
Jordan Hudgens <jordan@devcamp.com>
2017-06-22 14:40:29 +0000

    Integrated new and create actions for portfolios

--- DevcampPortfolio/app/controllers/portfolios_controller.rb
+++ DevcampPortfolio/app/controllers/portfolios_controller.rb
@@ -1,8 +1,21 @@
 class PortfoliosController < ApplicationController 
   def index
     @portfolio_items=Portfolio.all
-  <p><%= portfolio_items.title %><p>
-  <p><%= portfolio_items.subtitle %><p>
-  <p><%= portfolio_items.body %><p>
-  <%= image_tag portfolio_items.thumb_image %>
+  end
+  
+  
+  def new
+    @portfolio_items = Portfolio.new
+  end
+   def update
+    respond_to do |format|
+      if @blog.update(blog_params)
+        format.html { redirect_to @blog, notice: 'Your blog is now live.'
+        format.json { render :show, status: :ok, location: @blog }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+  
 end

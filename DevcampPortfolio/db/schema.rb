fef629525020c8794deda0941a10253c15114c34
Jordan Hudgens <jordan@devcamp.com>
2017-07-11 15:52:33 +0000

    Integrated database relationships between topics and blog posts

--- DevcampPortfolio/db/schema.rb
+++ DevcampPortfolio/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170710141230) do
+ActiveRecord::Schema.define(version: 20170711144735) do
 
   create_table "blogs", force: :cascade do |t|
     t.string   "title"
@@ -18,8 +18,10 @@ ActiveRecord::Schema.define(version: 20170710141230) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "slug"
-    t.integer  "status", default: 0
+    t.integer  "status"
+    t.integer  "topic_id"
     t.index ["slug"], name: "index_blogs_on_slug", unique: true
+    t.index ["topic_id"], name: "index_blogs_on_topic_id"
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
@@ -49,6 +51,12 @@ ActiveRecord::Schema.define(version: 20170710141230) do
     t.integer  "percent_utilized"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end

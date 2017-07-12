84c46629ab17f0dfe85ea2593f5a0d4510a3e92b
Jordan Hudgens <jordan@devcamp.com>
2017-07-12 17:15:19 +0000

    Implemented image defaults with portfolio items

--- DevcampPortfolio/app/controllers/blogs_controller.rb
+++ DevcampPortfolio/app/controllers/blogs_controller.rb
@@ -1,25 +1,31 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
+
   # GET /blogs
   # GET /blogs.json
   def index
     @blogs = Blog.all
   end
+
   # GET /blogs/1
   # GET /blogs/1.json
   def show
   end
+
   # GET /blogs/new
   def new
     @blog = Blog.new
   end
+
   # GET /blogs/1/edit
   def edit
   end
+
   # POST /blogs
   # POST /blogs.json
   def create
     @blog = Blog.new(blog_params)
+
     respond_to do |format|
       if @blog.save
         format.html { redirect_to @blog, notice: 'Your post is now live.' }
@@ -28,43 +34,47 @@ class BlogsController < ApplicationController
       end
     end
   end
+
   # PATCH/PUT /blogs/1
   # PATCH/PUT /blogs/1.json
   def update
     respond_to do |format|
       if @blog.update(blog_params)
         format.html { redirect_to @blog, notice: 'Blog was successfully updated.' }
-        format.json { render :show, status: :ok, location: @blog }
       else
         format.html { render :edit }
       end
     end
   end
+
   # DELETE /blogs/1
   # DELETE /blogs/1.json
   def destroy
     @blog.destroy
     respond_to do |format|
-      format.html { redirect_to blogs_url, notice: 'Blog was successfully destroyed.' }
+      format.html { redirect_to blogs_url, notice: 'Post was removed.' }
       format.json { head :no_content }
     end
   end
+
+  def toggle_status
+    if @blog.draft?
+      @blog.published!
+    elsif @blog.published?
+      @blog.draft!
+    end
+        
+    redirect_to blogs_url, notice: 'Post status has been updated.'
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_blog
-      @blog = Blog.find (params[:id])
+      @blog = Blog.friendly.find(params[:id])
     end
+
     # Never trust parameters from the scary internet, only allow the white list through.
     def blog_params
       params.require(:blog).permit(:title, :body)
     end
-end
-
-def toggle_status
-  if@blog.draft?
-      @blog.published! 
-      elsif @blog.published?
-      @blog.draft!
-end
-  redirect_to blogs_url, notice: 'Post status has been updated.'
 end
--- DevcampPortfolio/db/schema.rb
+++ DevcampPortfolio/db/schema.rb
@@ -15,10 +15,10 @@ ActiveRecord::Schema.define(version: 20170711144735) do
   create_table "blogs", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
     t.string   "slug"
-    t.integer  "status"
+    t.integer  "status",     default: 0
     t.integer  "topic_id"
     t.index ["slug"], name: "index_blogs_on_slug", unique: true
     t.index ["topic_id"], name: "index_blogs_on_topic_id"

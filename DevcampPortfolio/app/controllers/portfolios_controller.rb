770a30d72396df01fca062ae8a9a26b0b9e32a22
Jordan Hudgens <jordan@devcamp.com>
2017-07-12 15:32:27 +0000

    Implemented custom scopes for portfolio items

--- DevcampPortfolio/app/controllers/portfolios_controller.rb
+++ DevcampPortfolio/app/controllers/portfolios_controller.rb
@@ -3,11 +3,14 @@ class PortfoliosController < ApplicationController
     @portfolio_items=Portfolio.all
   end
   
+  def Angular
+    @angular_portfolio_items = Portfolio.angular
+  end
   
   def new
     @portfolio_items = Portfolio.new
   end
-   def update
+ def update
     respond_to do |format|
       if @blog.update(blog_params)
         format.html { redirect_to @blog, notice: 'Your blog is now live.'}
@@ -19,24 +22,17 @@ class PortfoliosController < ApplicationController
   
   def edit 
   end 
-  #Destroy/delete the record
-  @portfolio_items= Portfolio.find(params{:id})
   
-end
+  def show
+  @portfolio_items= Portfolio.find(params{:id})
+  end
+  
 # Redirect
- def destroy
+  def destroy
     @blog.destroy
     respond_to do |format|
       format.html { redirect_to blogs_url, notice: 'Post was removed.' }
     end
   end
   
-  <% @blog.each do %>
-      <tr>
-        <td><%= blog.title %></td>
-        <td><%= blog.body %></td>
-        <td><%= link_to 'Show', blog %></td>
-        <td><%= link_to 'Edit', edit_blog_path(blog) %></td>
-        <td><%= link_to 'Delete Post', blog, method: :delete, data: { confirm: 'Are you sure?' } %></td>
-      </tr>
-    <% end %>
+ end
--- DevcampPortfolio/db/seeds.rb
+++ DevcampPortfolio/db/seeds.rb
@@ -1,7 +1,17 @@
+3.times do |topic|
+  Topic.create!(
+    title:"Topic #{topic}"
+    )
+end
+
+puts "3 topics created"
+
+
 10.times do |blog|
   Blogs.create!(
     title:"My blog Post #{blog}"
-    body:"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."
+    body:"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
+    topic_id: Topic.last.id
     )
 end
 
@@ -16,14 +26,24 @@ end
 
 puts "5 skills created"
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
   Portfolio.create!(
     title: "Portfolio title #{portfolio_item}" ,
-    subtitle:" My great service" ,
+    subtitle:"Ruby on Rails" ,
     body:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." ,
     main_image:"http://via.placeholder.com/600x400" ,
     thumb_image:"http://via.placeholder.com/350x200" ,
     )
-  
+end
+
+1.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio title #{portfolio_item}" ,
+    subtitle:"Angular" ,
+    body:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." ,
+    main_image:"http://via.placeholder.com/600x400" ,
+    thumb_image:"http://via.placeholder.com/350x200" ,
+    )
+end
 
 puts "9 portfolio items created"

96a5a0d83ebb9135c6cb1d83c7879f2cdb4d5bce
Jordan Hudgens <jordan@devcamp.com>
2017-07-20 15:20:47 +0000

    Added guest user for current_user

--- DevcampPortfolio/app/controllers/application_controller.rb
+++ DevcampPortfolio/app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include DeviseWhitelist
   include SetSource
+  include CurrentUserSource
+  
+  def current_user
+    super || OpenStruct.new(name: "Guest User", first_name: "Guest", last_name: "User", email: "guest@example.com")
+  end
 end

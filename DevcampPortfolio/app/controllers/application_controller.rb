6ba926b37a64cd411fd03cb01b27712fd33b075a
Jordan Hudgens <jordan@devcamp.com>
2017-07-20 21:14:25 +0000

    Added nav bar and default header content

--- DevcampPortfolio/app/controllers/application_controller.rb
+++ DevcampPortfolio/app/controllers/application_controller.rb
@@ -2,9 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include DeviseWhitelist
   include SetSource
-  include CurrentUserSource
+  include CurrentUserConcern
   
-  def current_user
-    super || OpenStruct.new(name: "Guest User", first_name: "Guest", last_name: "User", email: "guest@example.com")
+  before_filter :set_title
+  
+  def set_title
+    @page_title = "DevcampPortfolio | My Portfolio Website"
   end
 end

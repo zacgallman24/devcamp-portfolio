4627b9f5b252ee4aff2f48d65d1644dda9fadf50
Jordan Hudgens <jordan@devcamp.com>
2017-08-02 17:08:20 +0000

    Refactored alerts into help page

--- DevcampPortfolio/app/helpers/application_helper.rb
+++ DevcampPortfolio/app/helpers/application_helper.rb
@@ -17,7 +17,7 @@ module ApplicationHelper
   end
 
   def copyright_generator
-    DevcampViewTool::Renderer.copyright 'Jordan Hudgens', 'All rights reserved'
+    DevcampViewTool::Renderer.copyright 'Zac Gallman', 'All rights reserved'
   end
 
   def nav_items
@@ -57,6 +57,18 @@ module ApplicationHelper
 
   def active? path
     "active" if current_page? path
+  end
+
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+
+    if alert
+      alert_generator alert
+    end
+  end
+
+  def alert_generator msg
+    js add_gritter(msg, title: "Zac Gallmans Portfolio", sticky: false)
   end
 
 end

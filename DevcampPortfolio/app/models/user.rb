9e4c3654b519e7d438456b7ba64692fd4e3dc928
Jordan Hudgens <jordan@devcamp.com>
2017-08-03 15:01:14 +0000

    Implemented redis library and created comment generator

--- DevcampPortfolio/app/models/user.rb
+++ DevcampPortfolio/app/models/user.rb
@@ -12,14 +12,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-         
-     validates_presence_of :name
-         
+
+  validates_presence_of :name
+
+  has_many :comments, dependent: :destroy
+
   def first_name
     self.name.split.first
   end
-  
+
   def last_name
-        self.name.split.last
+    self.name.split.last
   end
 end

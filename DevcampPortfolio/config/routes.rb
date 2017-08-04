987bbaf82571bcb1085199d051482b33c252a8e9
Jordan Hudgens <jordan@devcamp.com>
2017-08-04 02:18:58 +0000

    Created a broadcast with ActionCable

--- DevcampPortfolio/config/routes.rb
+++ DevcampPortfolio/config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :comments
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :portfolios, except: [:show] do
     put :sort, on: :collection
@@ -9,12 +8,15 @@ Rails.application.routes.draw do
 
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
+  get 'tech-news', to: 'pages#tech_news'
 
   resources :blogs do
     member do
       get :toggle_status
     end
   end
-  
+
+  mount ActionCable.server => '/cable'
+
   root to: 'pages#home'
 end

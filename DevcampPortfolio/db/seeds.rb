19842487d47822a726b6abd1fe83facb5d36b25c
Jordan Hudgens <jordan@devcamp.com>
2017-08-04 20:28:12 +0000

    Final Changes

--- DevcampPortfolio/db/seeds.rb
+++ DevcampPortfolio/db/seeds.rb
@@ -1,3 +1,23 @@
+User.create!(
+  email: "test@test.com",
+  password: "asdfasdf",
+  password_confirmation: "asdfasdf",
+  name: "Admin User",
+  roles: "site_admin"
+)
+
+puts "1 Admin user created"
+
+User.create!(
+  email: "test2@test.com",
+  password: "asdfasdf",
+  password_confirmation: "asdfasdf",
+  name: "Regular User",
+)
+
+puts "1 regular user created"
+
+
 3.times do |topic|
   Topic.create!(
     title: "Topic #{topic}"
@@ -50,7 +70,7 @@ puts "9 portfolio items created"
 3.times do |technology|
   Portfolio.last.technologies.create!(
     name: "Technology #{technology}"
-    )
+  )
 end
 
-puts "3 technologies items created"
+puts "3 technologies created"
